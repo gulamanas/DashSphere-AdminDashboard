331717d466553976bd77a2af5d1b27cfdf43381c
Gulam Anas <g.anas786@gmail.com>
2024-09-18 23:50:56 +0530

    static faq data

---
+++ src/data/mockfaqData.ts
@@ -0,0 +1,28 @@
+export const faqs = [
+  {
+    question: 'How do I add a new user to the system?',
+    answer:
+      'To add a new user, navigate to the "Users" section from the sidebar menu...',
+  },
+  {
+    question: 'How can I customize the dashboard layout?',
+    answer:
+      'You can customize the dashboard layout by accessing the "Settings" menu...',
+  },
+  {
+    question: 'What should I do if I forget my admin password?',
+    answer:
+      'If you forget your admin password, click on the "Forgot Password" link on the login page...',
+  },
+  {
+    question:
+      'How do I generate reports from the data available on the dashboard?',
+    answer:
+      'To generate reports, navigate to the "Reports" section from the main menu...',
+  },
+  {
+    question: 'How can I manage user roles and permissions?',
+    answer:
+      'To manage user roles and permissions, go to the "Users" section and select the user whose role you want to update...',
+  },
+];
--- src/scenes/faq/index.tsx
+++ src/scenes/faq/index.tsx
@@ -6,6 +6,7 @@ import AccordionDetails from '@mui/material/AccordionDetails';
 import Typography from '@mui/material/Typography';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { tokens } from '../../theme';
+import { faqs } from '../../data/mockfaqData';
 
 const FAQ = () => {
   const theme = useTheme();
@@ -13,72 +14,18 @@ const FAQ = () => {
   return (
     <Box m='20px'>
       <Header title='FAQ' subtitle='Frequently Asked Questions Page' />
-
-      <Accordion defaultExpanded>
-        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-          <Typography color={colors.greenAccent[500]} variant='h5'>
-            An Important Question
-          </Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion defaultExpanded>
-        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-          <Typography color={colors.greenAccent[500]} variant='h5'>
-            Another Important Question
-          </Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion defaultExpanded>
-        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-          <Typography color={colors.greenAccent[500]} variant='h5'>
-            Your Favorite Question
-          </Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion defaultExpanded>
-        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-          <Typography color={colors.greenAccent[500]} variant='h5'>
-            Some Random Question
-          </Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion defaultExpanded>
-        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-          <Typography color={colors.greenAccent[500]} variant='h5'>
-            The Final Question
-          </Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
+      {faqs.map((faq, index) => (
+        <Accordion key={index}>
+          <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+            <Typography color={colors.greenAccent[500]} variant='h5'>
+              {faq.question}
+            </Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>{faq.answer}</Typography>
+          </AccordionDetails>
+        </Accordion>
+      ))}
     </Box>
   );
 };
